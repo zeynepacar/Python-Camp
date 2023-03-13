661db4242f96086da46b213d58cda773a3bcaa1b
zeynepacar <acar.zeynep@hotmail.com>
2023-03-14 00:20:16 +0300

    added hw1

---
+++ HW-1/hw-1.py
@@ -0,0 +1,17 @@
+#Data Types
+#integer, float : sayısal veri tipi
+#string : karakter dizisi
+#list: aynı veya farklı veri tiplerini bir arada tutan liste, değiştirilebilir
+#tuple: listeye benzer yapıda ama değiştirilemez
+#dictionary: key-value çiftlerinden oluşan elemanları içerir
+#boolean: True veya False olarak iki değer alabilir
+
+
+#Kodlama.io sitesindeki kurs isimleri string veri tipindedir
+
+#Kurstaki bölümlerin tamamlanıp tamamlanmadığını görmek için şart blokları kullanılmıştır
+completed = False
+if completed == True:
+    print("Kurs tamamlandı!")
+else:
+    print("Kurs tamamlanmadı")
